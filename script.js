299539875350c88ed79da74289393214325f703f
Nitish Singh <89546196+TechnologyHell@users.noreply.github.com>
2023-12-22 13:38:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function showPrev() {
 }
 
 function updateGallery() {
-    
     const thumbnails = document.getElementsByClassName('thumbnail');
     const totalItems = thumbnails.length;
 
@@ -53,9 +52,9 @@ function updateGallery() {
     }
 }
 
-
 window.addEventListener('load', function () {
     updateGallery();
+    calculateShippingDate(); // Call the function to calculate and display the shipping date on load
 });
 
 document.getElementById('next-btn').addEventListener('click', function () {
@@ -71,3 +70,68 @@ document.getElementById('prev-btn').addEventListener('click', function () {
     }
     updateGallery();
 });
+
+
+function getShippingDate() {
+    var pincode = document.getElementById('pincode').value.trim(); // Trim to remove leading and trailing spaces
+    if (pincode === '') {
+        document.getElementById('shipping-date').innerText = '';
+        document.getElementById('shipping-message').style.display = 'none';
+        return;
+    }
+    var shippingDate = calculateShippingDate(); 
+    document.getElementById('shipping-date').innerText = shippingDate;
+    document.getElementById('shipping-message').style.display = 'block';
+}
+
+function calculateShippingDate() {
+    var today = new Date();
+    var shippingDate = new Date(today);
+    shippingDate.setDate(today.getDate() + 15); 
+
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    var formattedShippingDate = "Guranteed Delivered by \n" + shippingDate.toLocaleDateString('en-US', options);
+
+    return formattedShippingDate;
+}
+
+
+function updatePrice() {
+    var quantity = document.getElementById('quantity').value;
+
+    var basePrice = 899; 
+    if (quantity == 1){
+        var totalPrice = basePrice;
+    }
+    else if (quantity ==2){
+        var totalPrice = 1599;
+    }
+    else if (quantity ==3){
+        var totalPrice = 2199;
+    }
+    else if (quantity ==4){
+        var totalPrice = 2799;
+    }
+    else if (quantity ==5){
+        var totalPrice = 3299;
+    }
+    totalPrice = Math.round(totalPrice);
+    
+
+    document.getElementById('product-price').innerText = '₹ ' + totalPrice;
+}
+
+function shareContent() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Customizable YouTube Play Button',
+            text: 'Check out this amazing product!',
+            url: window.location.href
+        })
+            .then(() => console.log('Shared successfully'))
+            .catch((error) => console.error('Error sharing:', error));
+    } else {
+        alert('Web Share API is not supported in your browser. You can manually copy the link.');
+    }
+}
+
